577ee8ade7b8180540f72f610b6354d3bfcc880c
Alan Currie <alan.currie@realise.com>
2019-02-18 14:42:33 +0000

    :zap: added jest test coverage

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,9 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  collectCoverageFrom: ['src/*.vue'],
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{vue}', '!**/node_modules/**'],
+  coverageReporters: ['html', 'text-summary'],
   coverageThreshold: {
     'global': {
       'branches': 100,
